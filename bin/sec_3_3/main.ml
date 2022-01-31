746354d745e27f7f9a0c9ac87e704f4e4725f0b1
2or3 <t.nagashima.cog@gmail.com>
2022-01-31 23:36:23 +0900

    update practice_3_3

--- bin/sec_3_3/main.ml
+++ bin/sec_3_3/main.ml
@@ -3,5 +3,12 @@ let vol_cone =
   let base = pi *. 2.0 *. 2.0 in
   base *. 5.0 /. 3.0;;
 
+let f x =
+  let x3 = x * x * x in
+  let x3_1 = x3 + 1 in
+  x3 + x3_1;;
+
 print_float vol_cone;;
+print_string "\n";;
+print_int (f 5);;
 print_string "\n";;
