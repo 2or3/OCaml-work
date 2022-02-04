faeee58da5c18fda7bc3f4f1c7f2fac35c22debd
2or3 <t.nagashima.cog@gmail.com>
2022-02-05 00:19:52 +0900

    Add 3 5 2

--- bin/sec_3_5_2/main.ml
+++ bin/sec_3_5_2/main.ml
@@ -1,3 +1,9 @@
-(* Recursion *)
-print_int res;;
+(* Iteration *)
+let fact n =
+  let rec iterfact (i, res, n) =
+    if i = n then res
+    else iterfact (i+1, res * i, n) in
+  iterfact (1, 1, n);;
+
+print_int (fact 19);;
 print_string "\n";;
