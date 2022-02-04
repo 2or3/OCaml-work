ab6dfded7f9f903e16cf86821215bfcae97817db
2or3 <t.nagashima.cog@gmail.com>
2022-02-05 00:21:26 +0900

    Update iteration

--- bin/sec_3_5_2/main.ml
+++ bin/sec_3_5_2/main.ml
@@ -1,9 +1,10 @@
 (* Iteration *)
 let fact n =
-  let rec iterfact (i, res, n) =
+  let rec iterfact (i, res) =
     if i = n then res
-    else iterfact (i+1, res * i, n) in
-  iterfact (1, 1, n);;
+    else iterfact (i+1, res * i) in
+  iterfact (1, 1);;
 
 print_int (fact 19);;
 print_string "\n";;
+
