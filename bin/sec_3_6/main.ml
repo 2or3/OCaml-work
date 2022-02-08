4b42e235b6ff2bfe025cc2258d5ca4790b4e8337
2or3 <t.nagashima.cog@gmail.com>
2022-02-09 00:35:25 +0900

    proceed high-order function

--- bin/sec_3_6/main.ml
+++ bin/sec_3_6/main.ml
@@ -1,1 +1,16 @@
 (* high-order function *)
+let rec sum_of (f, n) =
+  if n = 0 then 0 else sum_of (f, n - 1) + f n;;
+
+let sum_of_square n =
+  let square x = x * x in
+  sum_of (square, n);;
+let sum_of_cube n =
+  let cube x = x * x * x in
+  sum_of (cube, n);;
+
+print_int (sum_of_square 5);;
+print_string "\n";;
+print_int (sum_of_cube 5);;
+print_string "\n";;
+
