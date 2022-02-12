a6ada8bdf8e19ba488997db292d01a6b0d32fbb2
2or3 <t.nagashima.cog@gmail.com>
2022-02-13 01:16:51 +0900

    proceed 3.6.5

--- bin/sec_3_6/main.ml
+++ bin/sec_3_6/main.ml
@@ -47,3 +47,26 @@ let (^-^) x y = x * 2 + y * 3;;
 print_int(9 ^-^ 6);;
 print_string "\n";;
 
+(* Newton-Raphson *)
+let deriv f =
+  let dx = 0.1e-10 in
+    fun x -> (f(x +. dx) -. f(x)) /. dx;;
+
+print_float (deriv (fun x -> x *. x *. x) 3.0);;
+print_string "\n";;
+
+let fixpoint f init =
+  let threshold = 0.1e-10 in
+  let rec loop x =
+    let next = f x in
+    if abs_float (x -. next) < threshold then x
+    else loop next
+  in loop init;;
+
+let newton_transform f = fun x -> x -. f(x) /. (deriv f x);;
+
+let newton_method f guess = fixpoint (newton_transform f) guess;;
+
+let square_root a = newton_method (fun x -> x *. x -. a) 1.0;;
+print_float (square_root 5.0);;
+print_string "\n";;
