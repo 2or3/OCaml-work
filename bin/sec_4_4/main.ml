f13d33c2323e24d202a3c5426475a8a599d8c43b
2or3 <t.nagashima.cog@gmail.com>
2022-02-24 00:00:36 +0900

    Resolve 4.1

--- bin/sec_4_4/main.ml
+++ bin/sec_4_4/main.ml
@@ -1,6 +1,10 @@
-(* Combinator *)
-let ($) f g x = f (g x);;
+(* Practice *)
+(* 4.1 *)
+let uncurry f (x, y) = f x y;;
+let average x y = (x +. y) /. 2.0;;
+let uncurried_avg = uncurry average;;
 
-let id x = x;;
-print_float ((sqrt $ id) 3.0);;
+print_float (average 3.0 5.0);;
+print_string "\n";;
+print_float (uncurried_avg (3.0, 5.0));;
 print_string "\n";;
