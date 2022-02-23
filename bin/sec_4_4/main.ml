59a1345181694155eeef35533148ac6b9e7f7ffe
2or3 <t.nagashima.cog@gmail.com>
2022-02-24 00:05:41 +0900

    Resolve 4.2

--- bin/sec_4_4/main.ml
+++ bin/sec_4_4/main.ml
@@ -8,3 +8,13 @@ print_float (average 3.0 5.0);;
 print_string "\n";;
 print_float (uncurried_avg (3.0, 5.0));;
 print_string "\n";;
+
+(* 4.2 *)
+let rec repeat f n x =
+  if n > 0 then repeat f (n - 1) (f x) else x;;
+
+let fib n =
+  let (fibn, _) = repeat (fun (a, b) -> (a + b, a)) n (1,0)
+  in fibn;;
+print_int (fib 10);;
+print_string "\n";;
