47525482b335789430452a70979b6bdcdc2b2600
2or3 <t.nagashima.cog@gmail.com>
2022-02-26 01:26:36 +0900

    Complete 5-1

--- bin/sec_5_1/main.ml
+++ bin/sec_5_1/main.ml
@@ -3,3 +3,9 @@ let list = [3; 9; 0; -10];;
 List.iter (Printf.printf "%d ") list;;
 print_string "\n";;
 
+let lt = [10; -11; 3; 5];;
+List.iter (Printf.printf "%d ") (9 :: lt);;
+print_string "\n";;
+
+List.iter (Printf.printf "%d ") ((::) (100, lt));;
+print_string "\n";;
