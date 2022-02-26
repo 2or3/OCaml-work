4a3a4a480908dca71794ba817ddbf5429769a33c
2or3 <t.nagashima.cog@gmail.com>
2022-02-27 00:27:26 +0900

    Proceed length

--- bin/sec_5_3/main.ml
+++ bin/sec_5_3/main.ml
@@ -1,1 +1,10 @@
 let list = 10 :: 1 :: 4 :: 2 :: 32 :: [];;
+
+(* length *)
+let rec length l =
+  match l with
+    [] -> 0
+  | _ :: rest -> 1 + length rest;;
+
+print_int (length list);;
+print_string "\n";;
