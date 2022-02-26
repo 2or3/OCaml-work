d7f050ff21bce4bf62305ec31c793a6e327b18d1
2or3 <t.nagashima.cog@gmail.com>
2022-02-27 00:16:15 +0900

    Complete 5-2

--- bin/sec_5_2/main.ml
+++ bin/sec_5_2/main.ml
@@ -1,1 +1,11 @@
-(* List *)
+(* List match *)
+let rec sum_list l =
+  match l with
+    [] -> 0
+  | n :: rest -> n + (sum_list rest);;
+
+print_int (sum_list [1; 5; 3; 9]);;
+print_string "\n";;
+print_int (sum_list (1 :: 4 :: 11 :: 9 :: []));;
+print_string "\n";;
+
