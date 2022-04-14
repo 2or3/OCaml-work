4737db2a37a9718512160d0a4d5f89f4357a63f2
2or3 <t.nagashima.cog@gmail.com>
2022-04-15 00:02:42 +0900

    add size & depth for b-tree

--- bin/sec_6_4/main.ml
+++ bin/sec_6_4/main.ml
@@ -1,2 +1,17 @@
 (* b-tree *)
+type 'a tree = Lf | Br of 'a * 'a tree * 'a tree;;
+let chartree = Br ('a', Br ('b', Br ('d', Lf, Lf), Lf), Br ('c', Br ('e', Lf, Lf), Br('f', Lf, Lf)));;
 
+let rec size = function
+    Lf -> 0
+  | Br (_, left, right) -> 1 + size left + size right;;
+
+print_int (size chartree);;
+print_string "\n";;
+
+let rec depth = function
+    Lf -> 0
+  | Br (_, left, right) -> 1 + max (depth left) (depth right);;
+
+print_int (depth chartree);;
+print_string "\n";;
